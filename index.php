040c2c18a60947962f598b2fef9bccd4ae9495ef
evlampiya <eva.shamgunova@mail.ru>
2022-05-14 16:41:52 +0200

    Task 2

--- index.php
+++ index.php
@@ -8,3 +8,21 @@ $order = &$very_bad_unclear_name;
 $order .= "Hello World!";
 
 echo "\nYour order is:$very_bad_unclear_name.";
+
+
+//Task 2
+
+$a = 132;
+echo $a . "<\n>";
+$b = 100;
+echo $b;
+
+$fl = 1.23;
+echo $fl . "\n";
+
+
+echo (8+4) . "<\n>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
