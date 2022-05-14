a2e56fbc8653a83c1481b3eb8b41bea1daf84759
evlampiya <eva.shamgunova@mail.ru>
2022-05-15 00:10:45 +0200

    Task 17

--- index.php
+++ index.php
@@ -191,3 +191,73 @@ function NumSum($num, $sum=0)
         NumSum($sum);
     else return;
 }
+
+
+// Task 17
+$XxX = "";
+$arrXxX = array();
+for($i=1;$i<5;$i++)
+{
+    $XxX = $XxX . "x";
+    array_push($arrXxX,$XxX);
+    echo $arr[$i] . "<br>";
+}
+
+function arrayFill($Symbol,$var)
+{
+    $temp = array();
+    for ($i = 0;$i < $var; $i++)
+    {
+        array_push($retArr,$Symbol);
+    }
+    return $temp;
+}
+
+$doubleArray = array(
+    array(1,2,3),
+    array(4,5),
+    array(6)
+);
+
+function SumCounter(array $array){
+    $Sum = array_sum($array);
+    foreach($array as $child) {
+        if (is_array($child))
+            $Sum += SumCounter($child);
+    }
+    return $Sum;
+}
+
+$Array3X3 = array();
+for($i = 0; $i < 3; $i++){
+    for($j = 0; $j < 3; $j++){
+        $Array3X3[$i][$j] = $i +3*$j +1;
+    }
+}
+
+
+$array2539 = array (2, 5, 3, 9);
+$result = $array2539[0]*$array2539[1]+$array2539[2]*$array2539[3];
+echo "<\n>" . $result . "<\n>";
+
+
+$user = array(
+    "surname" => "Shamgunova ",
+    "name" => "Eva ",
+    "patronymic" => "Maksimovna "
+);
+foreach ($user as $str)
+    echo $str . " ";
+
+$date = array(
+    "day" => 14,
+    "month" => 5,
+    "year" => 2022
+);
+echo $date["'year'"] . ":";
+echo $date["'month'"] . ":";
+echo $date["'day'"] . ":";
+
+$TaskArray = array('a','b','c','d','e');
+echo"<\n>" . count($TaskArray);
+echo"<\n>" . $TaskArray[count($TaskArray)-1] . " " . $TaskArray[count($TaskArray)-2];
